0d6551b03d883c3db86e5b2fc4e3284629adf8b7
Uberoy <joacim.jansson@iths.se>
2023-11-15 08:33:10 +0100

    Create SQLBolt_Excercises.sql

---
+++ SQLOvningarJoacim/SQLBolt_Excercises.sql
@@ -0,0 +1,75 @@
+--###SQLBolt###--
+--**Lesson 6**--
+--¤Task 3¤--
+--SELECT *
+--FROM Movies
+--INNER JOIN Boxoffice
+--    ON Movies.Id = Boxoffice.Movie_id
+--ORDER BY Rating DESC;
+
+--**Lesson 7**--
+--¤Task 1¤--
+--SELECT DISTINCT Building_name 
+--FROM employees
+--LEFT JOIN Buildings
+--    ON employees.Building = Buildings.Building_name;
+
+--¤Task 2¤--
+--SELECT Building_name, Capacity
+--FROM Buildings;
+
+--¤Task 3¤--
+--SELECT DISTINCT Building_name, Role 
+--FROM Buildings
+--LEFT JOIN Employees
+--    ON Buildings.building_name = Employees.Building;
+
+
+--**Lesson 8**--
+--¤Task 1¤--
+--SELECT * 
+--FROM employees
+--WHERE Building IS NULL;
+
+----¤Task 2¤--
+--SELECT DISTINCT Building_name 
+--FROM Buildings
+--LEFT JOIN Employees
+--    ON Buildings.Building_name = Employees.Building
+--WHERE Building IS NULL
+
+
+--**Lesson 9**--
+--¤Task 1¤--
+--SELECT Title, (Domestic_sales + International_sales) / 1000000 AS Sales 
+--FROM movies
+--INNER JOIN Boxoffice
+--    ON Id = Movie_id;
+
+--¤Task 2¤--
+--SELECT Title, Rating * 10 AS 'Rating in Percent'
+--FROM Movies
+--INNER JOIN Boxoffice
+--    ON Id = Movie_id;
+
+--¤Task 3¤--
+--SELECT *
+--FROM Movies
+--INNER JOIN Boxoffice
+--    ON Id = Movie_id
+--WHERE NOT Year%2
+    
+--**Lesson 10*--
+--¤Task 1¤--
+--SELECT MAX(Years_employed) 
+--FROM employees;
+
+----¤Task 2¤--
+--SELECT Role, AVG(Years_employed)
+--FROM Employees
+--GROUP BY Role;
+
+----¤Task 3¤--
+--SELECT Building, SUM(Years_employed)
+--FROM Employees
+--GROUP BY Building
