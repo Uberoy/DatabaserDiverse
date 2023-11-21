eae9238697c93c5009df22d2df580c749d186a87
Uberoy <joacim.jansson@iths.se>
2023-11-21 09:47:16 +0100

    Update SQLQuery1.sql

--- Labb1/SQLQuery1.sql
+++ Labb1/SQLQuery1.sql
@@ -2,7 +2,7 @@
 
 --USE BookStoreDb;
 
-BEGIN--Uppskapande av tabeller--
+BEGIN -- Uppskapande av tabeller
 CREATE TABLE AuthorTbl(
 ID int PRIMARY KEY,
 FirstName nvarchar(max) not null,
@@ -29,21 +29,34 @@ Amount int not null,
 PRIMARY KEY (StoreID, ISBN13));
 
 --1/4 extra tabell--
-CREATE TABLE CustomersTbl(
+CREATE TABLE CustomerTbl(
+UserName nvarchar(max) not null,
 FirstName nvarchar(max) not null,
 LastName nvarchar(max) not null,
 PhoneNumber int not null);
 
 --2/4 extra tabell--
+
+CREATE TABLE RegisteredUsersTbl(
+UserName nvarchar(max),
+RegisteredStoreID int FOREIGN KEY REFERENCES StoresTbl(ID));
+
+--3/4 extra tabell--
+CREATE TABLE ReviewTbl(
+ISBN13 bigint FOREIGN KEY REFERENCES BooksTbl(ISBN13),
+Score float);
+
+--4/4 extra tabell--
 CREATE TABLE OrderTbl(
 OrderID int PRIMARY KEY,
 StoreID int not null,
 Item int not null);
 --Jag tänker att item ska kopplas till ett unikt ID som böcker har och kanske lägga till andra produkter också och då blir ISBN en dålig kolumn att kolla på.
 --Antar att "Item" ska vara en FOREIGN KEY i så fall men osäker.
-END--REGION_END
 
-BEGIN--Populera exempel till AuthorTbl--
+END
+
+BEGIN -- Populera exempel till AuthorTbl
 INSERT INTO AuthorTbl 
 VALUES (1, 'Agatha', 'Christie', '1890-09-15 00:00:00.000');
 INSERT INTO AuthorTbl 
@@ -54,16 +67,7 @@ INSERT INTO AuthorTbl
 VALUES (4, 'Michel', 'Foucault', '1926-10-15 00:00:00.000');
 END
 
-BEGIN--Populera exempel till StoresTbl--
-INSERT INTO StoresTbl
-VALUES (1, 'Adlibris', 'Hittepågatan 15');
-INSERT INTO StoresTbl
-VALUES (2, 'Bokus', 'Fantasivägen 2');
-INSERT INTO StoresTbl
-VALUES (3, 'Akademibokhandeln', 'Låtsasröset 100');
-END
-
-BEGIN --Populera exempel till BooksTbl
+BEGIN -- Populera exempel till BooksTbl
 INSERT INTO BooksTbl
 VALUES (9783847267508, 'Der Einzige Und Sein Eigentum', 'Tyska', 375, '2012-05-11 00:00:00.000', 2);
 INSERT INTO BooksTbl
@@ -86,5 +90,138 @@ INSERT INTO BooksTbl
 VALUES (9780141195377, 'On the Genealogy of Morals', 'Engelska', 93, '2013-11-07 00:00:00.000', 3);
 END
 
-SELECT *
-FROM BooksTbl
+BEGIN -- Populera exempel till CustomerTbl
+INSERT INTO CustomerTbl
+VALUES ('joajan', 'Joacim','Jansson', 0738123456);
+
+INSERT INTO CustomerTbl
+VALUES ('nikhje', 'Niklas','Hjelm', 0738224466);
+END
+
+BEGIN -- Populera exempel till OrderTbl
+INSERT INTO OrderTbl
+VALUES (012345, 1,1337);
+END
+
+BEGIN -- Populera exempel till RegisteredUsersTbl
+
+INSERT INTO RegisteredUsersTbl
+VALUES ('joajan', 1);
+
+END
+
+BEGIN -- Populera exempel till ReviewTbl
+
+INSERT INTO ReviewTbl
+VALUES (9783847267508, 8.8);
+
+END 
+
+BEGIN -- Populera exempel till StockTbl
+
+INSERT INTO StockTbl
+VALUES (1, 9783847267508, 19);
+INSERT INTO StockTbl
+VALUES (1, 1620490072, 9);
+INSERT INTO StockTbl
+VALUES (1, 9789188745989, 100);
+INSERT INTO StockTbl
+VALUES (1, 9789175471679, 200);
+INSERT INTO StockTbl
+VALUES (2, 9780141195377, 2000);
+INSERT INTO StockTbl
+VALUES (2, 9780140441185, 2000);
+INSERT INTO StockTbl
+VALUES (3, 9780007527526, 1);
+INSERT INTO StockTbl
+VALUES (3, 9789189007130, 1);
+INSERT INTO StockTbl
+VALUES (3, 9783847267508, 1);
+INSERT INTO StockTbl
+VALUES (3, 1620490072, 1);
+INSERT INTO StockTbl
+VALUES (3, 9789188745989, 1);
+INSERT INTO StockTbl
+VALUES (3, 9789175471679, 1);
+INSERT INTO StockTbl
+VALUES (3, 9789175470900, 1);
+INSERT INTO StockTbl
+VALUES (3, 9780141395838, 1);
+
+END
+
+BEGIN -- Populera exempel till StoresTbl
+INSERT INTO StoresTbl
+VALUES (1, 'Adlibris', 'Hittepågatan 15');
+INSERT INTO StoresTbl
+VALUES (2, 'Bokus', 'Fantasivägen 2');
+INSERT INTO StoresTbl
+VALUES (3, 'Akademibokhandeln', 'Låtsasröset 100');
+END
+
+CREATE VIEW [TitlesPerAuthorView] AS
+SELECT	FirstName + ' ' + LastName AS [Name], 
+		DATEDIFF(YEAR, Birthday, GETDATE()) AS Age,
+		COUNT(AuthorID) AS NumberOfTitles,
+		SUM(BooksTbl.Price * StockTbl.Amount) AS TotalValue
+FROM AuthorTbl
+JOIN BooksTbl ON AuthorTbl.ID = BooksTbl.AuthorID
+JOIN StockTbl ON BooksTbl.ISBN13 = StockTbl.ISBN13 
+GROUP BY FirstName, LastName, Birthday; -- Skapa exempelvy som visar "Titlar per författare"
+
+CREATE PROCEDURE MoveBooksInStock 
+    @MoveFrom int, 
+    @MoveTo int, 
+    @Book bigint, 
+    @Amount int = 1
+AS
+BEGIN
+    BEGIN TRANSACTION;
+
+    DECLARE @CanMoveFrom BIT, @CanMoveTo BIT;
+
+    -- Check conditions for @MoveFrom
+    SELECT @CanMoveFrom = CASE WHEN Amount >= @Amount AND Amount > 0 THEN 1 ELSE 0 END
+    FROM StockTbl WITH (UPDLOCK)
+    WHERE StoreId = @MoveFrom AND ISBN13 = @Book;
+
+    -- Check if @MoveTo row exists
+    SELECT @CanMoveTo = CASE WHEN COUNT(*) > 0 THEN 1 ELSE 0 END
+    FROM StockTbl WITH (UPDLOCK)
+    WHERE StoreId = @MoveTo AND ISBN13 = @Book;
+
+    -- Move books from @MoveFrom
+    IF @CanMoveFrom = 1
+    BEGIN
+        UPDATE StockTbl
+        SET Amount = Amount - @Amount
+        WHERE StoreId = @MoveFrom AND ISBN13 = @Book;
+    END
+
+    -- Move books to @MoveTo only if books were successfully moved from @MoveFrom
+    IF @CanMoveFrom = 1
+    BEGIN
+        IF @CanMoveTo = 1
+        BEGIN
+            UPDATE StockTbl
+            SET Amount = Amount + @Amount
+            WHERE StoreId = @MoveTo AND ISBN13 = @Book;
+        END
+        ELSE
+        BEGIN
+            -- If @MoveTo row does not exist, consider an INSERT or handle accordingly
+            INSERT INTO StockTbl (StoreId, ISBN13, Amount)
+            VALUES (@MoveTo, @Book, @Amount);
+        END
+    END
+
+    -- Commit or Rollback the transaction
+    IF @@ERROR = 0
+        COMMIT;
+    ELSE
+        ROLLBACK;
+END;
+
+
+EXEC MoveBooksInStock @MoveFrom = 3, @MoveTo = 2, @Book = 1620490072, @Amount = 8
+
